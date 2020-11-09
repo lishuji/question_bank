e4e43086784dae9ebdf554d452d7967b5b6e324f
kane.li <kane.li@huolala.cn>
2020-11-09 20:04:23 +0800

    route config

---
+++ app/Http/Controllers/ManagerController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kankan
+ * Date: 2020-11-09
+ * Time: 19:45
+ */
+
+namespace App\Http\Controllers;
+
+
+class ManagerController extends Controller
+{
+    public function register()
+    {
+        return 000;
+    }
+
+    public function getUserInfo()
+    {
+        return 111;
+    }
+
+    public function editUserInfo()
+    {
+        return 222;
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ManagerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('manager/get_user_info', [ManagerController::class, 'getUserInfo']);
+Route::post('manager/edit_user_info', [ManagerController::class, 'editUserInfo']);
